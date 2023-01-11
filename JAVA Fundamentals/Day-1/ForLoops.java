9bc066c852eed6c0d1abfd056b4806c01621b832
Vishal-m <vishalmahesh.study@gmail.com>
2023-01-11 13:02:28 +0530

    Delete JAVA Fundamentals/Day-1 directory

--- JAVA Fundamentals/Day-1/ForLoops.java
+++
@@ -1,9 +0,0 @@
-package vishal;
-
-public class ForLoops {
-    public static void main(String[] args) {
-        for(int count = 1; count<=5; count++){
-            System.out.println(count);
-        }
-    }
-}

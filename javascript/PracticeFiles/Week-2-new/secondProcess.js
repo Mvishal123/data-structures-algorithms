b05bbd84a863d7a43e9ca5cb2b88bf1dcd649f7f
Vishal-m <vishalmahesh.study@gmail.com>
2023-10-04 22:48:41 +0530

    removed files

--- javascript/PracticeFiles/Week-2-new/secondProcess.js
+++
@@ -1,7 +0,0 @@
-const logFetch = (jsonBody) => console.log(jsonBody);
-
-
-const  sendObj = {
-    method: "GET"
-} 
-fetch("http://localhost:3000/postData?range=10", sendObj).then(result => console.log(result));
--- javascript/week-3/arrowFunctions.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("./hello.txt", "utf-8",(err, data) => {
-    console.log(data);
-});
